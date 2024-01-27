92c35f4e95fabeae387d5bfccdb7ec8ac9593630
Sakshi Jaiswal <144778485+thesakshijaiswal@users.noreply.github.com>
2024-01-27 08:24:49 +0530

    feat: throw error for invalid inputs

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ const principal = document.getElementById("principal");
 const rate = document.getElementById("rate");
 const year = document.getElementById("year");
 const result = document.getElementById("result");
+const evalBtn = document.getElementById("evalBtn");
 
+function evaluate() {
+  const p = parseFloat(principal.value);
+  const r = parseFloat(rate.value);
+  const n = parseFloat(year.value);
+  if (!isNaN(p) && !isNaN(r) && !isNaN(n)) {
+    const multiplication = (p * r * n)/100;
+    if (multiplication === 0) {
+      result.innerText = "⚠️Inputs should be non zero";
+    } else {
+      result.innerText = `₹ ${multiplication}`;     
+      result.style = "color: black;"
+    }
+  } else {
+    result.innerText = "⚠️ Invalid input";
+  }  
+}          
 
-function evaluate(p, r, n) {
-  return result.innerText = `₹ ${(p * r * n) / 100}`;
-}
-
-function results() {
-  evaluate(principal.value, rate.value, year.value)
-  result.style = "color: black"
-}
+evalBtn.addEventListener("click",evaluate);
